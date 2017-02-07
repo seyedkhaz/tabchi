3390f5096342f4088e3c89e637e737b53aacc1b6
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-02-08 01:12:13 +0330

    update

--- creator.lua
+++ creator.lua
@@ -1,6 +1,17 @@
 redis = (loadfile "redis.lua")()
-io.write("Enter Tabchi ID : ")
-local last = io.read()
+function gettabchiid()
+    local i, t, popen = 0, {}, io.popen
+    local pfile = popen('ls')
+	local last = 0
+    for filename in pfile:lines() do
+        if filename:match('tabchi%-(%d+)%.lua') and tonumber(filename:match('tabchi%-(%d+)%.lua')) >= last then
+			last = tonumber(filename:match('tabchi%-(%d+)%.lua')) + 1
+			end		
+    end
+    return last
+end
+local last = gettabchiid()
+io.write("Auto Detected Tabchi ID : "..last)
 io.write("\nEnter Full Sudo ID : ")
 local sudo=io.read()
 local text,ok = io.open("base.lua",'r'):read('*a'):gsub("TABCHI%-ID",last)
